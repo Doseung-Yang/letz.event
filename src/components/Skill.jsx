8804ef2aae7392d17a0bc410a2fd2708c378b4ba
unknown <doseung.yang@WADIZ.CO.KR>
2024-01-30 01:15:49 +0900

    commit tail

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -7,13 +7,29 @@ const Skill = () => {
             <div className="skill__inner">
                
                 <div className="skill__event">
-                    {skillText.map((skill, key) => (
-                        <div key={key}>
-                            <h3>{skill.title}</h3>
-                            <h1>{skill.subtitle}</h1>
-                            <p style={{ whiteSpace: 'pre-line' }}>{skill.subbtitle}</p>
+                    <div>
+                        <h3>{skillText[0].title}</h3>
+                        <h1>{skillText[0].subtitle}</h1>
+                        <p style={{ whiteSpace: 'pre-line' }}>{skillText[0].subbtitle}</p>
+                    </div>
+                    <div className="skill__special">
+                    <h1>{skillText[1].ftitle}</h1>
+                        <div className="skill__border">
+                            <div className="ftbox">{skillText[1].ftitle1}</div>
                         </div>
-                    ))} 
+
+                        <div className="skill__border">
+                            <div className="ftbox">{skillText[1].ftitle1}</div>
+                        </div>
+
+                        <div className="skill__border">
+                            <div className="ftbox">{skillText[1].ftitle1}</div>
+                        </div>
+
+                        <div className="skill__border">
+                            <div className="ftbox">{skillText[1].ftitle1}</div>
+                        </div>
+                    </div> 
                 </div>
             </div>
         </section>
--- src/constants/index.js
+++ src/constants/index.js
@@ -30,17 +30,17 @@ export const introText = [
 export const skillText = [
     {
         title: "event",
-       
-    },
-    {
         subtitle: "사전 오픈 이벤트",
-       
+        subbtitle: "19일부터 25일까지 프로젝트 오픈하고 \n특별한 혜택 받으세요",
     },
     {
-        subbtitle: "19일부터 25일까지 프로젝트 오픈하고 \n특별한 혜택 받으세요",
-      
+        ftitle: "혜택",
+        ftitle1: "혜택 1",
+        fsubtitle: "공간 와디즈 B1층 \n일주일 무료 대관",
+        feventtitle: "2000만원 상당",
+        fssubtitle: "최우수 프로젝트 1팀 선정"
     }
-]
+];
 
 export const siteText = [
     {
