a1eef9af51c461350aa0e73f338ceb11ae6f5704
unknown <doseung.yang@WADIZ.CO.KR>
2024-01-31 00:13:07 +0900

    map commit.!

--- src/pages/map.js
+++ src/pages/map.js
@@ -11,6 +11,12 @@ const MapPage = () => {
     script.onload = () => {
       new window.daum.Postcode({
         oncomplete: function (data) {
+          const message = data.address;
+
+       
+          if (window.webkit && window.webkit.messageHandlers) {
+            window.webkit.messageHandlers.iosListener.postMessage(message);
+          }
         },
       }).embed(postcodeRef.current);
     };
