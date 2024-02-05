78fa50a34de423b6dd9f644260e40a014ce75878
unknown <doseung.yang@WADIZ.CO.KR>
2024-02-05 11:43:44 +0900

    style commit #7

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,19 @@
-import Head from 'next/head'
 import "@/assets/scss/style.scss";
+
+export const metadata = {
+  title: "[와디즈] 누구나 도전하고 응원받을 수 있어요",
+  description: "사전 알림 신청하고 오픈 프로모션 혜택을 받으세요 🥰",
+  keywords: ["와디즈", "wadiz", "사전 알림", "렛즈", "letz", "사전 알림 신청", "프로모션", "와디즈 펀딩", "렛즈 베타 서비스 오픈", "사전 오픈 혜택"],
+  
+};
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <Head>
-        <title>[와디즈] 누구나 도전하고 응원받을 수 있어요</title>
+    <html lang="ko">
+      <head>
+      <link rel="icon" type="image/svg+xml" href="/favicon.png" />
+      <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
+      <title>[와디즈] 누구나 도전하고 응원받을 수 있어요</title>
         <meta name="description" content="사전 알림 신청하고 오픈 프로모션 혜택을 받으세요 🥰" />
         <meta name="keywords" content="와디즈, wadiz, 사전 알림, 렛즈, letz, 사전 알림 신청, 프로모션, 와디즈 펀딩, 렛즈 베타 서비스 오픈, 사전 오픈 혜택" />
         <meta property="og:url" content="https://campaign.letz.team/" />
@@ -15,8 +23,8 @@ export default function RootLayout({ children }) {
         <meta property="og:description" content="사전 알림 신청하고 오픈 프로모션 혜택을 받으세요 🥰" />
         <link rel="icon" type="image/svg+xml" href="/favicon.png" />
         <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
-      </Head>
-      {children}
-    </>
+      </head>
+      <body>{children}</body>
+    </html>
   );
 }
