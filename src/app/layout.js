8c432ced0578d04e7d808eba6dd31d6208e80435
양도승 <157574745+yangdoseung@users.noreply.github.com>
2024-03-11 16:00:25 +0900

    title commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
       <head>
       <link rel="icon" type="image/svg+xml" href="/favicon.png" />
       <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
-      <title>와디즈 응원펀딩 서비스, Letz</title>
+      <title>와디즈 팀 펀딩 서비스, Letz</title>
         <meta name="description" content="누구나 도전하고 응원받을 수 있어요" />
         <meta name="keywords" content="와디즈, wadiz, 사전 알림, 렛즈, letz, 사전 알림 신청, 프로모션, 와디즈 펀딩, 렛즈 베타 서비스 오픈, 사전 오픈 혜택" />
         <meta property="og:url" content="https://campaign.letz.team/" />
