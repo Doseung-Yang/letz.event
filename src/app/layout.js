196c48caa65028fc6896db422868f2100ba1d025
양도승 <157574745+yangdoseung@users.noreply.github.com>
2024-03-29 16:32:06 +0900

    Letz 프로모션 페이지 git계정 이동

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "@/assets/scss/style.scss";
 export const metadata = {
   title: "와디즈 팀 펀딩 서비스, 렛즈",
   description: "누구나 도전하고 응원받을 수 있어요",
-  keywords: ["와디즈", "wadiz", "사전 알림", "렛즈", "letz", "사전 알림 신청", "프로모션", "와디즈 펀딩", "렛즈 베타 서비스 오픈", "사전 오픈 혜택"],
+  keywords: ["와디즈, wadiz, 와디즈 사전 알림 페이지, 렛즈, letz, 사전 알림 신청, 프로모션, 와디즈 펀딩, 렛즈 베타 서비스 오픈, 사전 오픈 혜택, 와디즈 렛즈, 팀 펀딩 렛즈, 와디즈 렛즈가 뭐에요?"],
   
 };
 
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
       <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
       <title>와디즈 팀 펀딩 서비스, Letz</title>
         <meta name="description" content="누구나 도전하고 응원받을 수 있어요" />
-        <meta name="keywords" content="와디즈, wadiz, 사전 알림, 렛즈, letz, 사전 알림 신청, 프로모션, 와디즈 펀딩, 렛즈 베타 서비스 오픈, 사전 오픈 혜택, 와디즈 렛즈, 팀 펀딩 렛즈, 와디즈 렛즈가 뭐에요?" />
+        <meta name="keywords" content="와디즈, wadiz, 와디즈 사전 알림 페이지, 렛즈, letz, 사전 알림 신청, 프로모션, 와디즈 펀딩, 렛즈 베타 서비스 오픈, 사전 오픈 혜택, 와디즈 렛즈, 팀 펀딩 렛즈, 와디즈 렛즈 시작하기" />
         <meta property="og:url" content="https://campaign.letz.team/" />
         <meta name="google-site-verification" content="9V1dRrDK350pZsg-hN3BGjuS1MjgurZvt9Tg9fQpVUY" />
         <meta property="og:title" content="와디즈 팀 펀딩 서비스, 렛즈" />
